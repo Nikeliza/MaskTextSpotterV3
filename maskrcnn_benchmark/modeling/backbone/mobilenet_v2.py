fa9f7e141b5a1d280538fd66cfba0823cd5f7f0a
Nikeliza <nikeliza@mail.ru>
2020-12-16 19:22:14 +0300

    Update mobilenet_v2.py

--- maskrcnn_benchmark/modeling/backbone/mobilenet_v2.py
+++ maskrcnn_benchmark/modeling/backbone/mobilenet_v2.py
@@ -1,150 +1,96 @@
+import torch.nn as nn
+from maskrcnn_benchmark.modeling.make_layers import inverted_residual_sequence, conv2d_bn_relu6
 
-# adapted from @tonylins 
-# https://github.com/tonylins/pytorch-mobilenet-v2/
-# licensed under the  Apache License, Version 2.0, January 2004
-
-from torch import nn
-
-def conv_bn(inp, oup, stride):
-    return nn.Sequential(
-        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
-        nn.BatchNorm2d(oup),
-        nn.ReLU6(inplace=True)
-    )
-
-
-def conv_1x1_bn(inp, oup):
-    return nn.Sequential(
-        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
-        nn.BatchNorm2d(oup),
-        nn.ReLU6(inplace=True)
-    )
-
-
-class InvertedResidual(nn.Module):
-    def __init__(self, inp, oup, kernel_size, stride, padding, expand_ratio=0.2):
-        super(InvertedResidual, self).__init__()
-        self.stride = stride
-        assert stride in [1, 2]
-
-        hidden_dim = round(inp * expand_ratio)
-        self.use_res_connect = self.stride == 1 and inp == oup
-
-        if expand_ratio == 1:
-            self.conv = nn.Sequential(
-                # dw
-                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, padding, groups=hidden_dim, bias=False),
-                nn.BatchNorm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
-                # pw-linear
-                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(oup),
-            )
-        else:
-            self.conv = nn.Sequential(
-                # pw
-                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
-                # dw
-                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, 1, groups=hidden_dim, bias=False),
-                nn.BatchNorm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
-                # pw-linear
-                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(oup),
-            )
-
-    def forward(self, x):
-        if self.use_res_connect:
-            return x + self.conv(x)
-        else:
-            return self.conv(x)
-
-def _make_divisible(v, divisor, min_value=None):
-    """
-    This function is taken from the original tf repo.
-    It ensures that all layers have a channel number that is divisible by 8
-    It can be seen here:
-    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    :param v:
-    :param divisor:
-    :param min_value:
-    :return:
-    """
-    if min_value is None:
-        min_value = divisor
-    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
-    # Make sure that round down does not go down by more than 10%.
-    if new_v < 0.9 * v:
-        new_v += divisor
-    return new_v
 
 class MobileNetV2(nn.Module):
-    def __init__(self, cfg, n_class=1000, input_size=224, width_mult=1.0):
+    def __init__(self, args):
         super(MobileNetV2, self).__init__()
-        block = InvertedResidual#done
-        input_channel =  _make_divisible(32 * width_mult, 4 if width_mult == 0.1 else 8)#ok
-        last_channel = _make_divisible(1280 * width_mult, 4 if width_mult == 0.1 else 8) if width_mult > 1.0 else 1280
-        self.features = nn.ModuleList([conv_bn(3, input_channel, 2)])
-        interverted_residual_setting = [
-            # t, c, n, s
-            [1, 16, 1, 1],
-            [6, 24, 2, 2],
-            [6, 32, 3, 2],
-            [6, 64, 4, 2],
-            [6, 96, 3, 1],
-            [6, 160, 3, 2],
-            [6, 320, 1, 1],
-        ]#self.conf
 
-        # building first layer
-        assert input_size % 32 == 0
-        #input_channel = int(input_channel * width_mult)
-        self.last_channel = int(last_channel * width_mult) if width_mult > 1.0 else last_channel
-        
-        # building inverted residual blocks
-        for t, c, n, s in interverted_residual_setting:
-            output_channel = _make_divisible(c * width_mult, 4 if width_mult == 0.1 else 8)
-            for i in range(n):
-                if i == 0:
-                    self.features.append(block(input_channel, output_channel, 3, s, 1, expand_ratio=t))
-                else:
-                    self.features.append(block(input_channel, output_channel, 3, 1, 1, expand_ratio=t))
-                input_channel = output_channel
-        # building last several layers
-        self.features.append(conv_1x1_bn(input_channel, self.last_channel))
-        # make it nn.Sequential
-        #self.features = nn.Sequential(*self.features)
-        self._initialize_weights()
-        self._freeze_backbone(cfg.MODEL.BACKBONE.FREEZE_CONV_BODY_AT)
+        # s1, s2 = 2, 2
+        # if args.DOWNSAMPLING == 16:
+        #     s1, s2 = 2, 1
+        # elif args.DOWNSAMPLING == 8:
+        #     s1, s2 = 1, 1
 
-    def _freeze_backbone(self, freeze_at):
-        for layer_index in range(freeze_at):
-            for p in self.features[layer_index].parameters():
-                p.requires_grad = False
+        # Network is created here, then will be unpacked into nn.sequential
+        self.network_settings = [{'t': -1, 'c': 32, 'n': 1, 's': 1},
+                                 {'t': 1, 'c': 16, 'n': 1, 's': 1},
+                                 {'t': 6, 'c': 24, 'n': 2, 's': 1},
+                                 {'t': 6, 'c': 32, 'n': 3, 's': 2},
+                                 {'t': 6, 'c': 64, 'n': 4, 's': 2},
+                                 {'t': 6, 'c': 128, 'n': 3, 's': 2},
+                                 {'t': 6, 'c': 256, 'n': 3, 's': 2},
+                                 {'t': 6, 'c': 512, 'n': 1, 's': 2},
+                                 {'t': None, 'c': 1280, 'n': 1, 's': 1}]
+        self.num_classes = args.MODEL.ROI_BOX_HEAD.NUM_CLASSES - 1
 
+        ###############################################################################################################
+
+        # Feature Extraction part
+        # Layer 0
+        self.network = [
+            conv2d_bn_relu6(args.NUM_CHANNELS,
+                            int(self.network_settings[0]['c'] * args.WIDTH_MULTIPLIER),
+                            args.KERNEL_SIZE,
+                            self.network_settings[0]['s'], args.DROUPOUT_PROB)]
+
+        # Layers from 1 to 7
+        for i in range(1, 8):
+            self.network.extend(
+                inverted_residual_sequence(
+                    int(self.network_settings[i - 1]['c'] * args.WIDTH_MULTIPLIER),
+                    int(self.network_settings[i]['c'] * args.WIDTH_MULTIPLIER),
+                    self.network_settings[i]['n'], self.network_settings[i]['t'],
+                    args.KERNEL_SIZE, self.network_settings[i]['s']))
+
+        # Last layer before flattening
+        # self.network.append(
+        #     conv2d_bn_relu6(int(self.network_settings[7]['c'] * args.WIDTH_MULTIPLIER),
+        #                     int(self.network_settings[8]['c'] * args.WIDTH_MULTIPLIER), 1,
+        #                     self.network_settings[8]['s'],
+        #                     args.DROUPOUT_PROB))
+
+        ###############################################################################################################
+
+        # Classification part
+        # self.network.append(nn.Dropout2d(args.DROUPOUT_PROB, inplace=True))
+        # self.network.append(nn.AvgPool2d(
+        #     (args.IMG_HEIGHT // args.DOWNSAMPLING, args.IMG_WIDTH // args.DOWNSAMPLING)))
+        # self.network.append(nn.Dropout2d(args.DROUPOUT_PROB, inplace=True))
+        # self.network.append(
+        #     nn.Conv2d(int(self.network_settings[8]['c'] * args.WIDTH_MULTIPLIER), self.num_classes,
+        #               1, bias=True))
+
+        self.network = nn.Sequential(*self.network)
+
+        self.initialize()
+
+    # def forward(self, x):
+    #     # Debugging mode
+    #     # for op in self.network:
+    #     #     x = op(x)
+    #     #     print(x.shape)
+    #     x = self.network(x)
+    #     x = x.view(-1, self.num_classes)
+
+    #    return x
 
     def forward(self, x):
         outputs = []
-        for index,m in enumerate(self.features):
-            x = m(x)
-            # extract layers
-            if index in [3,6,13,18]:
+        # x = self.network(x)
+        for idx, stage_name in enumerate(self.network):
+            x = stage_name(x)
+            if idx in [10, 13, 16, 17]:
                 outputs.append(x)
         return outputs
 
-    def _initialize_weights(self):
+    def initialize(self):
+        """Initializes the model parameters"""
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, (2. / n) ** 0.5)
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.xavier_normal(m.weight)
                 if m.bias is not None:
-                    m.bias.data.zero_()
+                    nn.init.constant(m.bias, 0)
             elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                n = m.weight.size(1)
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
+                nn.init.constant(m.weight, 1)
+                nn.init.constant(m.bias, 0)

6a991fbbebcb41f2f52dd93fe7ef90d9d187439e
Nikeliza <nikeliza@mail.ru>
2021-05-24 09:55:29 +0300

    Update make_layers.py

--- maskrcnn_benchmark/modeling/make_layers.py
+++ maskrcnn_benchmark/modeling/make_layers.py
@@ -93,7 +93,7 @@ def make_fc(dim_in, hidden_dim, use_gn=False):
     return fc
 
 
-def conv_with_kaiming_uniform(use_gn=False, use_relu=False):
+def conv_with_kaiming_uniform(use_gn=False, use_relu=False, use_syncbn=False):
     def make_conv(
         in_channels, out_channels, kernel_size, stride=1, dilation=1
     ):
@@ -109,7 +109,9 @@ def conv_with_kaiming_uniform(use_gn=False, use_relu=False):
         # Caffe2 implementation uses XavierFill, which in fact
         # corresponds to kaiming_uniform_ in PyTorch
         nn.init.kaiming_uniform_(conv.weight, a=1)
-        if not use_gn:
+        if use_syncbn:
+            module.append(DistributedSyncBN(out_channels))
+        elif not use_gn:
             nn.init.constant_(conv.bias, 0)
         module = [conv,]
         if use_gn:
